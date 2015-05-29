4f0bcbc9619bba71e84959738a7a4ba90669875b
Hadar Gezel <hgezel@gmail.com>
2015-05-29 19:19:04 +0300

    added jquey validation to sign up form
    added validation to sign_up.php see the files let me know if you need
    explanations

--- sign_up.php
+++ sign_up.php
@@ -1,7 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.validate.min.js"></script>
+<script type="text/javascript" src="sign_up_validate.js"></script>
 <?php
 include('header.php');
+
+
 //We check if the form has been sent
-if(isset($_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['address'], $_POST['gender'], $_POST['birth_date'], $_POST['is_designer']))
+if(isset($_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['address'], $_POST['gender'], $_POST['birth_date']))//, $_POST['is_designer']))
 {
 			//We check if the email form is valid
 			if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',$_POST['email']))
@@ -60,10 +65,10 @@ if($form)
 {
 ?>
 <div class="content">
-    <form action="sign_up.php" method="post">
+    <form id="sign_up_form" action="sign_up.php" method="post">
         Please fill the following form to sign up:<br />
         <div class="center">
-        	<label for="email">Email</label><input type="text" name="email" value="<?php if(isset($_SESSION['email'])){echo htmlentities($_SESSION['email'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
+        	<label for="email">Email</label><input type="email" name="email" value="<?php if(isset($_SESSION['email'])){echo htmlentities($_SESSION['email'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
             <label for="first_name">First name:</label><input type="text" name="first_name" id="first_name" /><br />
             <label for="last_name">Last name:</label><input type="text" name="last_name" id="last_name" /><br />
             <label for="address">Address:</label><input type="text" name="address" id='address' /><br />
@@ -72,11 +77,11 @@ if($form)
             	<option selected="selected" value="FEMALE">FEMALE</option>
             	<option value="MALE">MALE</option>
 			</select><br/>            
-            <label for="birth_date">Birth Date:</label><input type="text" name="birth_date" id="birth_date"  /><br />
-            <!--INSERT ISDESIGNER FURTHER LOGICS--><input type="checkbox" name="is_designer" id="is_designer" value="0"/><br/>
+            <label for="birth_date">Birth Date:</label><input type="date" name="birth_date" id="birth_date"  /><br />
+            <!--INSERT ISDESIGNER FURTHER LOGICS--><label for="is_designer">Designer:</label><input type="checkbox" name="is_designer" id="is_designer" value="0"/><br/>
             <label for="avatar">Avatar:<span class="small">(optional)</span></label><input type="text" name="avatar" value="<?php if(isset($_POST['avatar'])){echo htmlentities($_POST['avatar'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
             <label for="description">Description:<span class="small">(optional)</span></label><input type="text" name="description" id="description"  /><br />
-            <label for="website_link">Web Site:<span class="small">(optional)</span></label><input type="text" name="website_link" id="website_link"  /><br />
+            <label for="website_link">Web Site:<span class="small">(optional)</span></label><input type="url" name="website_link" id="website_link"  /><br />
             <input type="submit" value="Sign up" />
 		</div>
     </form>
---
+++ sign_up_validate.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+$().ready(function(){
+	$("#sign_up_form").validate({
+		//debug:true,
+		rules:{
+			email:{
+				required: true,
+				email:true
+			},
+			first_name: "required",
+			last_name: "required",
+			address: "required",
+			gender: "required",
+			birth_date: {
+				required: true,
+				dateISO: true
+			},
+			website_link:{
+				url: true
+			} 
+			
+		},
+		messages:{
+			email:{
+				required: "Please enter your Email",
+				email: "Please enter a valid Email"
+			},
+			first_name: "Please enter your first name",
+			last_name: "Please enter your last name",
+			address: "Please enter your address",
+			gender: "Please select your gender",
+			birth_date:{
+				required: "Please enter your birth date",
+				dateISO: "Please enter a valid date"
+			},
+			website_link:{
+				url: "Please enter a valid URL"
+			}
+		}
+	});
+});
