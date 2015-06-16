b4a4c18a7d67da54f6b61807570b110e0a0fd86a
amitabir <amabir@gmail.com>
2015-06-16 13:23:08 +0300

    Changed Item->item

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ $url_root = 'http://www.example.com/';
 
 //Loading Classes
 require_once 'User.php';
-require_once 'Item.php';
+require_once 'item.php';
 
 
 /******************************************************
